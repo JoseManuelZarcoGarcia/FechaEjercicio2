8602c5d043c8af94f569be7e8a1337b7f823370b
DESKTOP-A37MTN2\csi22-jzarcia <jose.manuel.zarco.garcia@gmail.com>
2024-02-05 12:01:02 +0100

    no message

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace FechaEjercicio2
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.Now;
+            dateTime.ToString("yy - MM - ddThh:mm: ss.ms");
+            Console.WriteLine("Formato fecha - tiempo: " + dateTime.ToString());
+
+            int dia = dateTime.Day;
+            int mes = dateTime.Month;
+            int anyo = dateTime.Year;
+            int hora = dateTime.Hour;
+            int minutos = dateTime.Minute;
+            int segundos = dateTime.Second;
+            string diaSemana = dateTime.DayOfWeek.ToString();
+            string diaAnyo = dateTime.DayOfYear.ToString();
+
+            Console.WriteLine(dia);
+            Console.WriteLine(mes);
+            Console.WriteLine(anyo);
+            Console.WriteLine(hora);
+            Console.WriteLine(minutos);
+            Console.WriteLine(segundos);
+            Console.WriteLine(diaSemana);
+            Console.WriteLine(diaAnyo);
+
+        }
+
+    }
+
+}
